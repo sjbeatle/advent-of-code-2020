96b2eba192e5bc9d01a9bb24aeb46cb70f46f04c
Steven Black <sblack@us.ibm.com>
2020-12-07 13:24:49 -0500

    day seven part one completed

---
+++ src/day/07/a/index.ts
@@ -0,0 +1,87 @@
+import { bagRules } from './input';
+
+const myBag = 'shiny gold';
+const rules: Rule[] = [];
+
+bagRules
+  .map(rule => {
+    const parentChildren = rule.split(' bags contain ');
+    const parent = parentChildren[0];
+    const children = parentChildren[1]
+      .slice(0, -1)
+      .split(', ')
+      .map((child): Bag => {
+        const [count, ...color] = child.split(' ');
+
+        return {
+          count: parseInt(count, 10) || 0,
+          color: color.join(' ').replace(/\sbags?$/, ''),
+        };
+      });
+
+    rules.push({
+      color: parent,
+      children,
+    })
+  });
+
+const validBags: Rule[] = rules.filter(rule => {
+  return getChildren(rule.color, []);
+});
+
+/**
+ * Answer
+ */
+console.log(`There are ${validBags.length} bags that can contain my bag.`);
+
+
+/**
+ * Helper Functions
+ */
+function getChildren(color: string, tracker: string[]) {
+  /**
+   * Guards
+   */
+  if (tracker.includes(myBag)) {
+    return true;
+  }
+
+  /**
+   * Recursion
+   */
+  let children: Bag[] = [];
+
+  rules.some(r => {
+    if (r.color === color) {
+      children = r.children;
+      return true;
+    };
+  });
+
+  const ruleChildren = children.map(child => {
+    return child.color;
+  });
+
+  const validChildren: boolean[] = ruleChildren.map(child => {
+    if (!tracker.includes(child)) { // we're not already looking for this color
+      tracker.push(child);
+      return getChildren(child, tracker);
+    }
+  });
+
+  return validChildren.includes(true);
+}
+
+
+/**
+ * Interfaces
+ */
+interface Rule {
+  color: string;
+  children: Bag[];
+}
+
+interface Bag {
+  color: string;
+  count: number;
+}
