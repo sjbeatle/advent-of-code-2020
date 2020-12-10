7c71cd673df6d8b8ca1888d27848bb3b946972e9
Steve Black <stevenjblack@gmail.com>
2020-12-10 12:52:19 -0500

    day 10 part one updates

--- src/day/10/a/index.ts
+++ src/day/10/a/index.ts
@@ -4,15 +4,15 @@ const sortedJoltages = outputJoltages.sort((a, b) => a - b);
 sortedJoltages.unshift(0); // prepend the outlet
 sortedJoltages.push(sortedJoltages[sortedJoltages.length -1] + 3); // append the device
 
-const joltDiffs: number[] = [];
+let joltDiffs = '';
 sortedJoltages.forEach((joltage, i) => {
   if (i === 0)
     return;
 
-  joltDiffs.push(joltage - sortedJoltages[i - 1]);
+  joltDiffs = joltDiffs + (joltage - sortedJoltages[i - 1]);
 });
 
-const diffOne = joltDiffs.filter(d => d === 1).length;
-const diffThree = joltDiffs.filter(d => d === 3).length;
+const diffOne = joltDiffs.match(/1/g).length;
+const diffThree = joltDiffs.match(/3/g).length;
 
 console.log(`The number of 1-jolt differences(${diffOne}) multiplied by the number of 3-jolt differences(${diffThree}) is:\n${diffOne * diffThree}`);
